d711927a3cf3054efc14650e83bff2ad63b59d2c
maicom-yamakishi <maicom.yamakishi@gmail.com>
2024-08-20 15:57:06 -0300

    first commit

---
+++ str-producer/src/test/java/com/yamakishi/str_producer/StrProducerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yamakishi.str_producer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StrProducerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
